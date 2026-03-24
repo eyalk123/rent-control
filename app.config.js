95ab861a97ee565775ac71fd8d9de654601b5cc7
eyalk123 <eyalkook@gmail.com>
2026-03-24 12:46:41 +0200

    Add EAS configuration, update package.json and package-lock.json for expo-dev-client, and enhance permissions in settings.local.json

--- app.config.js
+++ app.config.js
@@ -9,6 +9,9 @@ module.exports = {
     extra: {
       apiUrl: process.env.EXPO_PUBLIC_API_URL || 'http://localhost:8000',
       supportsRTL: true,
+      eas: {
+        projectId: '751de006-44a2-4bd6-b675-70e0b5af8517',
+      },
     },
   },
 };
